ad898a328fd3ce257e7ca37bf894a13d1e6f1290
netneko <cecizucchino@gmail.com>
2021-09-29 09:06:18 -0300

    Ejercicio clase 2

---
+++ C2/js/main.js
@@ -0,0 +1,28 @@
+
+function  calcularIMC ()
+{
+    var peso=parseInt(document.getElementById('peso').value);
+    console.log(peso);
+    var altura=parseInt(document.getElementById('altura').value);
+    let alt=altura / 100;
+    let IMC= peso / (alt * alt);
+    let clasif;
+    if(IMC < 18.49)
+    {
+        clasif='Peso bajo';
+    }
+    else if (IMC < 24.9)
+    {
+        clasif='Peso normal';
+    }
+    else if((IMC > 25)&&(IMC < 29.9))
+    {
+        clasif='Sobrepeso';
+    }
+    else
+    {
+        clasif='Obesidad';
+    }
+    alert("Su IMC es : " + IMC + " y estas con : " + clasif); 
+}
+
