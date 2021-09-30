c6275ba0b50f022626255b34b5f24e0ca35cd43e
netneko <cecizucchino@gmail.com>
2021-09-30 02:00:36 -0300

    Conversor de pesos arg a pesos chilenos

--- C4-Simulador/js/main.js
+++ C4-Simulador/js/main.js
@@ -1,11 +1,11 @@
 
 function  clpToArs ()
 {
-    var clp=parseInt(document.getElementById('clp').value);
+    let clp=parseInt(document.getElementById('clp').value);
     /* Paso 1: Pasar CLP a USD */
     let usd = clp / 811.70; /* Este valor tendria que ser dinamico consultando un WS */ 
     /* Paso 2 : Pasar de USD a ARS */
-    let ars= usd * 98.60; /* Este valor tendria que ser dinamico consultando un WS */
+    let ars= usd * 98.5130 /* Este valor tendria que ser dinamico consultando un WS */
     /* Paso 3: Calcular impuesto */
     let imp= ars * 0.65;
     /* Paso 4 : Sumar el valor del impuesto al valor en ARS */
@@ -13,3 +13,10 @@ function  clpToArs ()
     alert("El importe en pesos Argentinos (ARS) es : " + Math.round(total) + " $ de los cuales " + Math.round(imp) + "$ son de impuestos"); 
 }
 
+function arsToClp ()
+{
+    let ars=parseInt(document.getElementById('ars').value);
+    let usd= ars / 98.5130; /* Este valor tendria que ser dinamico consultando un WS */
+    let clp= usd * 811.70; /* Este valor tendria que ser dinamico consultando un WS */ 
+    alert("El importe en pesos Chilenos (CLP) es : " + Math.round(clp));
+}
