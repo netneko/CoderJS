57ed3c222ec1cf3a8d71ccaf3e7e3677b3ed4d8b
netneko <cecizucchino@gmail.com>
2021-11-03 18:58:19 -0300

    entrega 2 TP

--- Segunda Entrega TP/js/Eventos.js
+++ Segunda Entrega TP/js/Eventos.js
@@ -39,11 +39,7 @@ function guardarCarrito(carrito)
     if (carrito.length!=0) //Si el carrito no esta vacio, guardo lo que tiene adentro en el localStorage
     {
         localStorage.setItem('prodsEnCarrito',JSON.stringify(carrito));
+        const prodStorage= JSON.parse(localStorage.getItem('prodsEnCarrito'));
+        console.log(prodStorage);
     }
-    else if(JSON.parse(localStorage.getItem('prodsEnCarrito').length) > 0)
-        { //Si el carrito NO esta vacio, recupero los datos y los parseo de JSON a un objeto JS
-            const prodStorage= JSON.parse(localStorage.getItem('prodsEnCarrito'));
-            console.log(prodStorage);
-        }
-
 }
